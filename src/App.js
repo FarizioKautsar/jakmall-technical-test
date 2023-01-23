c9e00993783a39c6e67ab16dd3df6e1ae6137d51
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 22:18:15 +0700

    Root app fix

--- src/App.js
+++ src/App.js
@@ -6,23 +6,28 @@ import DeliveryPage from './pages/DeliveryPage';
 import PaymentPage from './pages/PaymentPage';
 import FinishPage from './pages/FinishPage';
 import JMStepper from './components/JMStepper';
+import { JMCard } from './components';
 
 function App() {
   const [step, setStep] = useState(STEPS.DELIVERY);
+  const [deliveryData, setDeliveryData] = useState(null);
+  const [paymentData, setPaymentData] = useState(null);
   
   return (
-    <>
+    <JMCard>
       <JMStepper
         step={step}
       />
-      {
-        step === STEPS.DELIVERY ? (
-          <DeliveryPage setStep={setStep}/>
-        ) : step === STEPS.PAYMENT ? (
-          <PaymentPage setStep={setStep}/>
-        ) : <FinishPage setStep={setStep}/>
-      }
-    </>
+      <div style={{ marginTop: 24, width: "100%" }}>
+        {
+          step === STEPS.DELIVERY ? (
+            <DeliveryPage setStep={setStep}/>
+          ) : step === STEPS.PAYMENT ? (
+            <PaymentPage setStep={setStep}/>
+          ) : <FinishPage setStep={setStep}/>
+        }
+      </div>
+    </JMCard>
   );
 }
 
