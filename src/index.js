06dcb197bbc2b5596bfb8129a03ebd6bafcec5a2
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 18:17:13 +0700

    GlobalStyle comp fix

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GlobalStyle from './components/GlobalStyle';
+import { JMContainer } from './components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle>
+    <GlobalStyle/>
+    <JMContainer>
       <App />
-    </GlobalStyle>
+    </JMContainer>
   </React.StrictMode>
 );
 
