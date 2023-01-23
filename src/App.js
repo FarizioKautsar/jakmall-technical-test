764d6b5b3be54d179501781b36dd0b1000b74789
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 18:00:58 +0700

    Init React app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import { STEPS } from './global/constants';
+
+function App() {
+  const [step, setStep] = useState(STEPS.DELIVERY);
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
