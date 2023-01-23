c49a65e08819321f178c4b3657994a65db0a3712
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 18:53:57 +0700

    Color definitions

---
+++ src/components/jmColors.jsx
@@ -0,0 +1,11 @@
+const jmColors = {
+  lightYellow: "#FFFAE6",
+  orange: "#FF8A00",
+  lightWhite1: "#EEEEEE",
+  lightWhite2: "#CCCCCC",
+  lightWhite3: "#D8D8D8",
+  green: "#1BD97B",
+  darkBlue: "#2D2A40",
+};
+
+export default jmColors;
