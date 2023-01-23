6f483e9fb21f8d9ebd7e99bc8f59ba5cdfad0c2e
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 18:03:42 +0700

    Init pages

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { STEPS } from './global/constants';
+import DeliveryPage from './pages/DeliveryPage';
+import PaymentPage from './pages/PaymentPage';
+import FinishPage from './pages/FinishPage';
 
 function App() {
   const [step, setStep] = useState(STEPS.DELIVERY);
+  
   return (
     <>
+      {
+        step === STEPS.DELIVERY ? (
+          <DeliveryPage setStep={setStep}/>
+        ) : step === STEPS.PAYMENT ? (
+          <PaymentPage setStep={setStep}/>
+        ) : <FinishPage setStep={setStep}/>
+      }
     </>
   );
 }
---
+++ src/global/constants.js
@@ -0,0 +1,5 @@
+export const STEPS = {
+  DELIVERY: "DELIVERY",
+  PAYMENT: "PAYMENT",
+  FINISH: "FINISH",
+}
