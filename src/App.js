c119cbe3a7f7a0abeb8fe4d8cd851841c9ddc312
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 23:23:54 +0700

    Local storage implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { STEPS } from './global/constants';
 import DeliveryPage from './pages/DeliveryPage';
 import PaymentPage from './pages/PaymentPage';
@@ -12,7 +11,40 @@ function App() {
   const [step, setStep] = useState(STEPS.DELIVERY);
   const [deliveryData, setDeliveryData] = useState(null);
   const [paymentData, setPaymentData] = useState(null);
+  const [orderId, setOrderId] = useState(null);
+
+  // STORING STATES TO LOCAL STORAGE
+  useEffect(() => {
+    if (step && step !== STEPS.DELIVERY) localStorage.setItem('step', JSON.stringify(step));
+  }, [step]);
+
+  useEffect(() => {
+    if (deliveryData) localStorage.setItem('deliveryData', JSON.stringify(deliveryData));
+  }, [deliveryData]);
+
+  useEffect(() => {
+    if (paymentData) localStorage.setItem('paymentData', JSON.stringify(paymentData));
+  }, [paymentData]);
+
+  useEffect(() => {
+    if (orderId) localStorage.setItem('orderId', JSON.stringify(orderId));
+  }, [orderId]);
+
+  // GETTING DATA FROM LOCAL STORAGE INTO STATE
+  useEffect(() => {
+    const lsStep = JSON.parse(localStorage.getItem('step'));
+    const lsDeliveryData = JSON.parse(localStorage.getItem('deliveryData'));
+    const lsPaymentData = JSON.parse(localStorage.getItem('paymentData'));
+    const lsOrderId = JSON.parse(localStorage.getItem('orderId'));
+
+    
+    if (lsStep) setStep(lsStep);
+    if (lsDeliveryData) setDeliveryData(lsDeliveryData);
+    if (lsPaymentData) setPaymentData(lsPaymentData);
+    if (lsOrderId) setOrderId(lsOrderId);
+  }, [])
   
+  console.log(step);
   return (
     <JMCard>
       <JMStepper
@@ -21,10 +53,10 @@ function App() {
       <div style={{ marginTop: 24, width: "100%" }}>
         {
           step === STEPS.DELIVERY ? (
-            <DeliveryPage setStep={setStep}/>
+            <DeliveryPage setStep={setStep} deliveryData={deliveryData} setDeliveryData={setDeliveryData}/>
           ) : step === STEPS.PAYMENT ? (
-            <PaymentPage setStep={setStep}/>
-          ) : <FinishPage setStep={setStep}/>
+            <PaymentPage setStep={setStep} isDropship={deliveryData?.sendAsDropshipper} setPaymentData={setPaymentData}/>
+          ) : <FinishPage paymentData={paymentData} setStep={setStep} orderId={orderId} setOrderId={setOrderId}/>
         }
       </div>
     </JMCard>
