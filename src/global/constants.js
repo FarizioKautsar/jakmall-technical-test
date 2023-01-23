9d3c72f90b90bfd42d7c988df54ccbceb99f5758
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 21:17:43 +0700

    Added constants for expeditions and payments

--- src/global/constants.js
+++ src/global/constants.js
@@ -3,3 +3,15 @@ export const STEPS = {
   PAYMENT: "PAYMENT",
   FINISH: "FINISH",
 };
+
+export const EXPEDITIONS = {
+  GO_SEND: 'GO_SEND',
+  JNE: 'JNE',
+  PERSONAL: 'PERSONAL',
+}
+
+export const PAYMENTS = {
+  E_WALLET: 'E_WALLET',
+  BANK_TRANSFER: 'BANK_TRANSFER',
+  VIRTUAL_ACCOUNT: 'VIRTUAL_ACCOUNT',
+}
--- src/global/enums.js
+++ src/global/enums.js
@@ -3,3 +3,15 @@ export const STEPS_TEXTS = {
   PAYMENT: "Payment",
   FINISH: "Finish",
 };
+
+export const EXPEDITIONS_TEXTS = {
+  GO_SEND: 'Go-Send',
+  JNE: 'JNE',
+  PERSONAL: 'Personal Courier',
+}
+
+export const PAYMENTS_TEXTS = {
+  E_WALLET: 'e-Wallet',
+  BANK_TRANSFER: 'Bank Transfer',
+  VIRTUAL_ACCOUNT: 'Virtual Account',
+}
