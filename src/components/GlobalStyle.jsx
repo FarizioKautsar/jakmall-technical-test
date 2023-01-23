013f86dab2a030b13777becc71008d1835e8d631
FarizioKautsar <kheruzy@gmail.com>
2023-01-23 18:01:23 +0700

    Added global styles

---
+++ src/components/GlobalStyle.jsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Inter', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Montserrat', sans-serif;
+  }
+`
+
+export default GlobalStyle;
